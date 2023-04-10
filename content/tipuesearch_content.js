803e28cf4fb0b65896d738c001be4b981a5d6826
41023315 <41023315@nfu.edu.tw>
2023-04-10 15:17:37 +0800

    加上 cmsimde 目錄架構, 並且修改網頁標題, 然後以 token 進行 push

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'admin login', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': '關於', 'text': '倉庫： https://github.com/mdewcm2023/site-41023315 \n 網站： : https://mdewcm2023.github.io/site-41023315 \n \n', 'tags': '', 'url': '關於.html'}, {'title': '權限', 'text': '近端倉儲的設定檔案:倉儲目錄下 .git 中的 config, 代表倉儲的 configuration file (設定檔案) \n', 'tags': '', 'url': '權限.html'}, {'title': '提交', 'text': 'commit \n git commit -m \n message \n 指令執行時，隨後要接上提交字串，說明本次提交的改版說明', 'tags': '', 'url': '提交.html'}]};
